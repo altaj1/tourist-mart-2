0783880887700176ef93a10051e2df80bb9ab34c
altaj1 <altaj1019@gmail.com>
2024-09-18 16:09:43 +0600

    devaging code for find in error

--- src/app/allProductData/api/route.js
+++ src/app/allProductData/api/route.js
@@ -7,5 +7,15 @@ export const GET =async (request, {params})=>{
     const { searchParams } = new URL(request.url);
     const searchText = searchParams.get("searchText")
     console.log("GET all dada from server", searchText)
+
+    try {
+        const query = {
+            name:{ $regex: String(searchText), $options: 'i' }
+        }
+        const result = await productsCollection.find(query).toArray()
+        
+    } catch (error) {
+        
+    }
     return NextResponse.json("data not found")
 }
