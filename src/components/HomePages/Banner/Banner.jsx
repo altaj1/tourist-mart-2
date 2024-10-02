92e436f7413d81ad2cf3d4d7ba749aba267b828b
altaj1 <altaj1019@gmail.com>
2024-10-02 17:54:07 +0600

    update banner

--- src/components/HomePages/Banner/Banner.jsx
+++ src/components/HomePages/Banner/Banner.jsx
@@ -3,11 +3,9 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="bg-gradient-to-r from-[#344617] to-[#5b792b] ">
-      {/* <div className="bg-[url('/images/banner3.png')] lg:h-[537px] md:h-[537px] h-80 bg-cover bg-no-repeat flex items-center justify-center bg-opacity-40">
-      
-      </div> */}
-      <div className="bg-gray-100 font-[sans-serif] relative  rounded overflow-hidden">
+    <div className="bg-[url('/images/pngtree-blue-and-fresh-flat-children-s-day-banner-background-picture-image_1617909.jpg')]">
+     
+      <div className=" font-[sans-serif] relative  rounded overflow-hidden">
         <div className="grid sm:grid-cols-2 gap-6 max-sm:gap-12 py-12 px-6">
           <div className="bg-gradient-to-tr from-[#273284b6] to-[#1e1e60] px-4 py-8 text-center rounded-[30px] w-full max-w-[300px] h-max skew-x-[10deg] mx-auto shadow-lg shadow-purple-400">
             <h6 className="text-gray-300 text-4xl">Big</h6>
@@ -24,7 +22,7 @@ const Banner = () => {
             <h6 className="text-2xl text-orange-500 mt-0.5">
               Limited Time Deal
             </h6>
-            <p className="text-gray-800 text-base mt-4">
+            <p className="text-[#000000] text-base mt-4">
               Discover amazing discounts and deals. Don&apos;t miss out on our
               exclusive offers <br /> available for a limited time.
             </p>
